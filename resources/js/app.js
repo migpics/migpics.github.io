353c6c5a36e800c352bc55ddb4b22bab6f3081c1
Miguel Valenzuela <migpics@gmail.com>
2018-12-10 20:25:21 +0100

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -143,11 +143,7 @@ $(document).ready(
            var preCode = ["[[\"Layer",{\"applyMatrix\":true}],[\"Layer\",{\"applyMatrix\":true,\"children\":[[\"Path\",{\"applyMatrix\":true,\"data\":{\"color":0,\"isPolygonal\":true},\"segments\":[ "];
            var drawingCode = preCode.concat(myPBPCode).join('\n\n'); //Change this to PBP Information
           
-           var endCode = ["\
-                          ],
-                            "\closed"\:true,
-					        "\strokeColor"\:[1,0.91765,0.49412],
-					        "\strokeWidth"\:5,
+           var endCode = ["],\"closed\":true,\"strokeColor\":[1,0.91765,0.49412],\"strokeWidth\":5,
 					        "\strokeCap"\:"\round"\,
 					        "\miterLimit"\:1
                           }]]}]]
